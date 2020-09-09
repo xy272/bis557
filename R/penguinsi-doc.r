a969a01da636d20bf7d8973ad72816142b61754c
xy272 <xining.yuan@yale.edu>
2020-09-09 00:46:27 -0400

    ommented out tidy creation of penguinsi.
    :wq

--- R/penguinsi-doc.r
+++ R/penguinsi-doc.r
@@ -5,6 +5,7 @@
 #' @rdname penguinsi
 #' @description
 #' The penguins data set after imputing all miss values using the {missForest} package.
+#' Note: all values are imputed, and it's usually a bad idea to impute dependent variables in an analysis.
 #' @references See the Palmerpenguins::penguins documentation for information about the values.
 #' @keywords data
 NULL
